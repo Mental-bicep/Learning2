15b3fee49c10a764dc62818e96725800ac34dbae
Ankit <ankitkumarmbill1@gmail.com>
2026-08-04 01:55:32 +0530

    added some code EurekaServer and EurekaClient

---
+++ EurekaServer/src/test/java/com/kodewala/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodewala.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
